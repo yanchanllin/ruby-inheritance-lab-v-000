dd409566a23229790ef3b44a45512c790daf7366
YanC Lin <yonalin1@gmail.com>
2017-11-20 18:54:41 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,16 @@
 class User
 
-  attr_accessor :first_name, :last_name
+  # attr_accessor :first_name, :last_name
 
-  def initialize(first_name,last_name)
-    @first_name = first_name
-    @last_name = last_name
+  # def initialize(first_name,last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+   def first_name
+     self.first
+   end
+  def last_name
+    self.last
   end
 
 
